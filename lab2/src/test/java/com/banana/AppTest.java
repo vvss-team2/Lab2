61aa9f6bf5f8cb4eebfe31300e68b11ce5d7756d
Pascu-Victor <me@vpascu.ro>
2026-03-17 17:24:35 +0200

    moar tests

--- lab2/src/test/java/com/banana/AppTest.java
+++ lab2/src/test/java/com/banana/AppTest.java
@@ -7,14 +7,17 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldDoAnono() {
+        assertTrue(!false);
     }
 }
